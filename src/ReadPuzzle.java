89abff37b75d291b8b172ff6e61ac94d9c7ee360
Gagandeep Randhawa <gagandeep.randhawa@akana.com>
2016-10-22 19:31:51 -0700

    Project initialization

---
+++ src/ReadPuzzle.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+
+/**
+ * Created by Gagandeep.Randhawa on 10/22/2016.
+ */
+public class ReadPuzzle {
+
+
+}
--- src/SlitherlinkSolver.java
+++ src/SlitherlinkSolver.java
@@ -1,8 +1,68 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.channels.InterruptedByTimeoutException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by Gagandeep.Randhawa on 10/22/2016.
  */
 public class SlitherlinkSolver {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
+        int rowCount = 0;
+        int colCount = 0;
 
+        //Read File and Add to String Buffer
+        StringBuilder sb = null;
+        Path path = Paths.get(System.getProperty("user.dir") + "\\src\\puzzle.txt");
+        FileInputStream in = new FileInputStream(path.toFile());
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        try {
+            sb = new StringBuilder();
+            String line = br.readLine();
+            colCount = (line.length() / 2) + 1;
+            while (line != null) {
+                rowCount++;
+                sb.append(line);
+                line = br.readLine();
+            }
+        }
+        finally {
+            br.close();
+        }
+
+        //Remove "," and replace "?" with 7 to store to integer matrix
+        //Reason to replace ? with 7, is to make sure we are using integer matrix
+        //No additional step of converting each character to integer everytime we check for constraints
+        String noCommas = sb.toString().replaceAll(",", "");
+        String cellValues = noCommas.replaceAll("\\?", "7");
+        char[] everything = cellValues.toCharArray();
+
+        //Create matrix based on row and col count of the file.
+        int[][] matrix = new int[rowCount][colCount];
+        int c = 0;
+        for(int i=0; i<rowCount; i++){
+            for(int j=0; j<colCount; j++){
+                String s = ""+everything[c];
+                matrix[i][j] = Integer.parseInt(s);
+                c++;
+            }
+        }
+
+        for(int i=0; i<rowCount; i++){
+            for(int j=0; j<colCount; j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        //Final Row and Column count to be provided to the solver
+        System.out.println("Rows = " + rowCount );
+        System.out.println("Columns = " + colCount);
     }
+
 }
+
+
+
